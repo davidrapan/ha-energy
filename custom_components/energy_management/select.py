500723a4301959eb5766c67f9075a96dd23e12e0
David Rapan <david@rapan.cz>
2026-04-01 21:53:40 +0200

    Add option to plan consumption based on daily mean
    Signed-off-by: David Rapan <david@rapan.cz>

--- custom_components/energy_management/select.py
+++ custom_components/energy_management/select.py
@@ -16,11 +16,28 @@ _LOGGER = getLogger(__name__)
 async def async_setup_entry(_: HomeAssistant, config_entry: ConfigEntry[Coordinator], async_add_entities: AddEntitiesCallback):
     _LOGGER.debug(f"async_setup_entry: {config_entry}")
 
-    async_add_entities([StrategyNowSelectEntity(config_entry.runtime_data)])
+    async_add_entities([StrategySelectEntity(config_entry.runtime_data), StrategyNowSelectEntity(config_entry.runtime_data)])
 
 class EnergyManagementSelectEntity(EnergyManagementEntity, SelectEntity):
     def update_options(self, value: str | None):
         self.coordinator.hass.config_entries.async_update_entry(self.coordinator.config_entry, options = {**self.coordinator.config_entry.options} | {self._attr_key: value})
+
+class StrategySelectEntity(EnergyManagementSelectEntity):
+    def __init__(self, coordinator):
+        self._attr_key = "strategy"
+        self._attr_name = "Strategy"
+        self._attr_entity_category = EntityCategory.CONFIG
+        self._attr_options = ["hourly", "daily_mean"]
+        super().__init__(coordinator)
+
+    @property
+    def current_option(self):
+        return self.coordinator.config_strategy
+
+    async def async_select_option(self, value: str):
+        self.coordinator.config_strategy = value
+        self.update_options(self.coordinator.config_strategy)
+        self.async_write_ha_state()
 
 class StrategyNowSelectEntity(EnergyManagementSelectEntity):
     def __init__(self, coordinator):
